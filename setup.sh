b2dd30a97de7004e69cc97b3ddaa62a0cff89651
Skip Miller <skip@thinktankconsulting.net>
2024-08-08 12:36:48 -0500

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,7 @@
+cd /Users/skip.miller/Library/CloudStorage/Dropbox/Development/ResumeDev
+git init
+git remote add origin https://github.com/skipmiller/ResumeDev.git
+git add .
+git commit -m "Initial commit"
+git branch -M main
+git push -u origin main
